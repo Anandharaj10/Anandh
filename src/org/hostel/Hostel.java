24593a2e435fc954088b837504bbf3c2680a3d96
Anandharaj10 <anandharajpap1096@gmail.com>
2022-03-09 21:19:18 +0530

    abc

---
+++ src/org/hostel/Hostel.java
@@ -0,0 +1,15 @@
+package org.hostel;
+
+public class Hostel {
+
+	public static void main(String[] args) {
+		int petrol = 100;
+		int diesel = 90;
+		if (petrol >= diesel) {
+			System.out.println("petrol is greater than deiesel");
+		} else {
+			System.out.println("diesel is lesser than petrol");
+		}
+	}
+
+}
